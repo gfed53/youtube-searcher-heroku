67a0e839fd5a90b9977112086c3077cff46dd278
gfed53 <gfederico7153@gmail.com>
2016-06-09 16:18:15 -0400

    Added scroll-to-top button

--- src/app.js
+++ src/app.js
@@ -7,11 +7,27 @@ angular
 }])
 
 .run(function(){
-	// function($) { 
-	// 	$.fn.goTo = function() {
-	// 	 $(this)[0].scrollIntoView(true); return this;
-	// 	   } 
-	// 	})(jQuery);
+	$(function(){
+	 
+		$(document).on( 'scroll', function(){
+	 
+			if ($(window).scrollTop() > 100) {
+				$('.scroll-top-wrapper').addClass('show');
+			} else {
+				$('.scroll-top-wrapper').removeClass('show');
+			}
+		});
+
+		$('.scroll-top-wrapper').on('click', scrollToTop);
+
+		function scrollToTop() {
+			verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
+			element = $('body');
+			offset = element.offset();
+			offsetTop = offset.top;
+			$('html, body').animate({scrollTop: offsetTop}, 500, 'linear');
+		}
+	})
 })
 
 
