cee1917c0eb48e489aa397f78b9b34f182f76d8d
gfed53 <gfederico7153@gmail.com>
2016-06-25 17:30:37 -0400

    Menu now fixed to top when scrolled to

--- src/app.js
+++ src/app.js
@@ -10,6 +10,12 @@ angular
 .run(['$timeout', '$rootScope', function($timeout, $rootScope){
 	
 	$timeout(function(){
+		// fixedAdjust();
+		fixedAdjustMenu();
+
+	}, 1000);
+
+	function fixedAdjust(){
 		var pageHeader = document.getElementById('page-header');
 		var main = document.getElementById('header-wrapper');
 		var header = document.getElementById('mast-header');
@@ -22,12 +28,40 @@ angular
 		pageHeader.className = 'fixed';
 		pageHeader.style.height = height+20+'px';
 		content.style.top = height+20+'px';
-	}, 1000);
+	}
+
+	function fixedAdjustMenu(){
+		var pageHeader = document.getElementById('page-header'),
+		main = document.getElementById('header-wrapper'),
+		header = document.getElementById('mast-header'),
+		credit = document.getElementById('credit'),
+		content = document.getElementById('animate-view-container'),
+		menu = document.getElementById('header-menu'),
+		style = window.getComputedStyle(credit),
+		creditMargin = style.getPropertyValue('margin-top');
+		creditMargin = creditMargin.replace('px', '');
+		creditMargin = creditMargin*2;
+		console.log(menu.offsetHeight); //42
+
+		console.log(creditMargin); //56
+		var headerHeight = header.offsetHeight+20+credit.offsetHeight+creditMargin,
+		menuHeight = menu.offsetHeight;
+		console.log(main.offsetHeight); //156
+		var height = main.offsetHeight;
+		document.onscroll = function(){
+			if(window.scrollY > headerHeight){
+				menu.style.height = menuHeight+'px';
+				menu.className = 'fixed';
+				console.log(menu.style.height);
+			} else {
+				menu.className = '';
+			}
+			// console.log(window.scrollY); //202
+		}
+	}
 
 	$rootScope.$on('$stateChangeSuccess', function(){
-		// console.log(main.offsetHeight);
-		// var header = document.getElementById('mast-header');
-		// console.log(header.offsetHeight);
+		window.scrollTo(0,0);
 	})
 	
 }])
