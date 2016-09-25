3e2ef8a64aa311a0de015d77c88ed21a416c9060
gfed53 <gfederico7153@gmail.com>
2016-09-25 17:46:22 -0400

    Replaced some old jQuery code with Angular

--- src/partials/header/header-partials/menuController.js
+++ src/partials/header/header-partials/menuController.js
@@ -6,12 +6,12 @@ angular
 function MenuCtrl($rootScope, ytVideoItems){
 	var vm = this;
 	vm.videoId = ytVideoItems.services.getVideoId();
+	vm.videoActive = false;
 
 	//Once we switch to the video state (by clicking on a video to watch), the video tab will now be visible from now on, so we have access to it for the duration of the session
 	$rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
 		if(toState.name === 'video'){
-			//TODO: Replace jQuery with Ang
-			$('#video-tab').show();
+			vm.videoActive = true;
 			vm.videoId = ytVideoItems.services.getVideoId();
 		}
 	});
